47c15a6736ef7174742569b9238ee6090d3e263f
mihailsgo <mihails.gordijenko@gmail.com>
2024-09-17 23:56:45 +0300

    Update commands.sh

--- digital-stamping-service/commands.sh
+++ digital-stamping-service/commands.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 pcscd --disable-polkit
-utnm -c "set autoconnect=true  84.50.77.2 1"
-utnm -c "set autoconnect=true  84.50.77.2 2"
+utnm -c "set autoconnect=true  85.51.88.3 1"
+utnm -c "set autoconnect=true  85.51.88.3 2"
 java --add-exports=jdk.crypto.cryptoki/sun.security.pkcs11.wrapper=ALL-UNNAMED -jar /opt/app/dmss-digital-stamping-service.jar
